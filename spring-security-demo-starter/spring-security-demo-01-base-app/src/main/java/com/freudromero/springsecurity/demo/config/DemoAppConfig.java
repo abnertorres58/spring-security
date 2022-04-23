7bef2df5437efa2f5d2062728efdce822a2d4af6
fred <abnertorres58@gmail.com>
2022-04-22 23:57:42 -0400

    Spring Security - create Spring Java Config class and Initializer class fix configuration

---
+++ spring-security-demo-starter/spring-security-demo-01-base-app/src/main/java/com/freudromero/springsecurity/demo/config/DemoAppConfig.java
@@ -0,0 +1,25 @@
+package com.freudromero.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.freudromero.springsecurity.demo")
+public class DemoAppConfig {
+
+    // Define a bean for the ViewResolver
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
